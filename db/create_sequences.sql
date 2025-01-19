96541c1a3fd61f0e1dab191bf64e792576b05c88
PavlosPT <pavlospt@PavlosPTs-Mac-mini.local>
2025-01-19 14:16:55 +0200

    Escape sequence name when comparing values
    Fixes #69

---
+++ db/create_sequences.sql
@@ -0,0 +1,29 @@
+-- Sequence for product table
+CREATE SEQUENCE product_seq START
+WITH
+    1 INCREMENT BY 1;
+
+-- Sequence for country table
+CREATE SEQUENCE country_seq START
+WITH
+    1 INCREMENT BY 1;
+
+-- Sequence for city table
+CREATE SEQUENCE city_seq START
+WITH
+    1 INCREMENT BY 1;
+
+-- Sequence for store table
+CREATE SEQUENCE store_seq START
+WITH
+    1 INCREMENT BY 1;
+
+-- Sequence for users table
+CREATE SEQUENCE user_seq START
+WITH
+    1 INCREMENT BY 1;
+
+-- Sequence for status_name table
+CREATE SEQUENCE "status-name_seq" START
+WITH
+    1 INCREMENT BY 1;
